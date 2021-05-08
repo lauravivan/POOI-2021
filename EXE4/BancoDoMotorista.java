942d09ac42ddd6ad8e827536baf8c769ec5e81ab
Laura Vivan Gonçalves <64754203+Laura-a11y@users.noreply.github.com>
2021-05-07 21:06:23 -0300

    Add files via upload

---
+++ EXE4/BancoDoMotorista.java
@@ -0,0 +1,98 @@
+package EXE4;
+
+public class BancoDoMotorista {
+    private int posicao;
+    private boolean totalmenteAbaixado;
+    private boolean totalmenteLevantado;
+    private boolean abaixado;
+    private boolean levantado;
+    
+    //Quando a posição se encontra em 0 = TOTALMENTE ABAIXADO
+    //Quando a posição se encontra em 1 ou 2 = ABAIXADO
+    //Quando a posição se encontra em 3 ou 4 = LEVANTADO
+    //Quando a posição se encontra em 5 = TOTALMENTE LEVANTADO
+    
+    public BancoDoMotorista() {
+    }
+
+    public void setPosicao(int posicao) {
+        if (posicao >= 0 && posicao <= 5) {
+            this.posicao = posicao;
+        } else {
+            System.out.println("Error message: Posição Inválida! Tente outra vez!");
+        }
+    }
+    
+    public int getPosicao() {
+        if (this.posicao >= 0 && this.posicao <= 5) {
+            System.out.println("Posição se encontra em " + this.posicao + ".");
+        }
+        return this.posicao;
+    }
+
+    public boolean isTotalmenteAbaixado() {
+        if (this.posicao != 0) {
+            this.totalmenteAbaixado = false;
+            System.out.println("Não, o banco não está totalmente abaixado pois "
+                    + "não se encontra na posição ZERO.");
+        }
+        if (this.posicao == 0) {
+            this.totalmenteAbaixado = true;
+            System.out.println("Sim, o banco está totalmente abaixado! Portanto, "
+                    + "não force a alavanca para baixo!");
+        }
+        return this.totalmenteAbaixado;
+    }
+    
+    public boolean isTotalmenteLevantado() {
+        if (this.posicao != 5) {
+            this.totalmenteLevantado = false;
+            System.out.println("Não, o banco não está totalmente levantado pois "
+                    + "não se encontra na posição CINCO.");
+        }
+        if (this.posicao == 5) {
+            this.totalmenteLevantado = true;
+            System.out.println("Sim, o banco está totalmente levantado! Portanto, "
+                    + "não force a alavanca para cima!");
+        }
+        return this.totalmenteLevantado;
+    }
+    
+    public boolean isAbaixado() {
+        if (this.posicao == 1 || this.posicao == 2 || this.posicao == 0) {
+            System.out.println("Sim, o banco está abaixado pois se encontra na "
+                    + "posição " + this.posicao);
+        } else {
+            System.out.println("Não, o banco não está abaixado pois está na posição " + this.posicao + ".");
+        }
+        return this.abaixado;
+    }
+
+    public boolean isLevantado() {
+        if (this.posicao == 3 || this.posicao == 4 || this.posicao == 5) {
+            System.out.println("Sim, o banco está levantado pois se encontra na "
+                    + "posição " + this.posicao);
+        } else {
+            System.out.println("Não, o banco não está levantado pois está na posição " + this.posicao + ".");
+        }
+        return this.levantado;
+    }
+    
+    public void abaixarBanco() {
+        if (this.posicao == 0) {
+            System.out.println("Não force a alavanca, o banco não pode mais ser abaixado!");
+        } else {
+            System.out.println("Você abaixou o banco!");
+            this.posicao -= 1;
+        }
+    }
+    
+    public void subirBanco() {
+        if (this.posicao == 5) {
+            System.out.println("Não force a alavanca, o banco não pode mais ser levantado!");
+        } else {
+            System.out.println("Você subiu o banco!");
+            this.posicao += 1;
+        } 
+    }
+}
