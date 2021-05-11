2a2d93e75830ed2d25dff059a3a24697b7b4c682
Laura Vivan Gonçalves <64754203+Laura-a11y@users.noreply.github.com>
2021-05-11 18:51:27 -0300

    Update EXE5.java

--- EXE5/EXE5.java
+++ EXE5/EXE5.java
@@ -3,12 +3,13 @@ package exe5;
 public class EXE5 {
 
     public static void main(String[] args) {
-        Lutador[] L = new Lutador[5];
+        Lutador[] L = new Lutador[6];
         
-        L[0] = new Lutador("Putscript", "Brasil", 29, 1.68f, 57.8f, 14, 2, 3);
-        L[1] = new Lutador("Snapshadow", "EUA", 35, 1.65f, 80.9f, 12, 2, 1);
-        L[2] = new Lutador("Dead Code", "Austrália", 28, 1.93f, 81.6f, 13, 0, 2);
-        L[3] = new Lutador("Ufocobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
-        L[4] = new Lutador("Nerdaard", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);  
+        L[0] = new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9f, 11, 2, 1);
+        L[1] = new Lutador("Putscript", "Brasil", 29, 1.68f, 57.8f, 14, 2, 3);
+        L[2] = new Lutador("Snapshadow", "EUA", 35, 1.65f, 80.9f, 12, 2, 1);
+        L[3] = new Lutador("Dead Code", "Austrália", 28, 1.93f, 81.6f, 13, 0, 2);
+        L[4] = new Lutador("Ufocobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
+        L[5] = new Lutador("Nerdaard", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);  
     }
 }
