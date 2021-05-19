141ccdceb9b9c6b383b1f8b13f93e06cfde9c715
Laura Vivan Gonçalves <64754203+Laura-a11y@users.noreply.github.com>
2021-05-19 17:35:26 -0300

    Delete EXESPY directory

--- EXESPY/atividade2.py
+++
@@ -1,8 +0,0 @@
-valorInicial = 10000
-taxa = 0.6 / 100
-tempoRendimento = 8
-
-valorFinal = valorInicial * ((1 + taxa) ** tempoRendimento)
-
-print("Seu valor final é: ", valorFinal)
-print("Fim do programa!")
--- EXESPY/atividade4.py
+++
@@ -1,6 +0,0 @@
-n = [0,1,2,3,4,5,6,7,8,9,10]
-
-for i in n:
-  if (i % 2) == 0:
-    print(i)
-print("Fim do programa!")
