8086399c607d3687715ecb83892f4f9abbd1b6ef
Laura Vivan Gonçalves <64754203+Laura-a11y@users.noreply.github.com>
2021-05-19 14:57:48 -0300

    Update Lista01N02.java

--- lista01/Lista01N02.java
+++ lista01/Lista01N02.java
@@ -2,14 +2,14 @@ import javax.swing.JOptionPane;
 
 public class Lista01N02 {
 	
-	/* 2) Um cinema deseja saber a opini„o dos espectadores sobre o filme que est·
-	sendo exibido. Cada espectador, respondeu a um question·rio, contendo sua
-	idade e sua opini„o sobre o filme: (1)Ûtimo, (2)bom, (3)regular, (4)ruim,
-	(5)pÈssimo. Elaborar um algoritmo que, lendo estes dados, calcule e escreva:
-	- a quantidade de respostas ”timo,
-	- a diferenÁa de votos entre respostas Bom e Regular,
-	- a mÈdia de idade das pessoas que responderam ruim,
-	- a percentagem de respostas PÈssimo
+	/* 2) Um cinema deseja saber a opini√£o dos espectadores sobre o filme que est√°
+	sendo exibido. Cada espectador, respondeu a um question√°rio, contendo sua
+	idade e sua opini√£o sobre o filme: (1)√≥timo, (2)bom, (3)regular, (4)ruim,
+	(5)p√©ssimo. Elaborar um algoritmo que, lendo estes dados, calcule e escreva:
+	- a quantidade de respostas √ìtimo,
+	- a diferen√ßa de votos entre respostas Bom e Regular,
+	- a m√©dia de idade das pessoas que responderam ruim,
+	- a percentagem de respostas P√©ssimo
 	Obs: Foram ouvidos 20 clientes. */
 
 	public static void main(String[] args) {
@@ -23,11 +23,11 @@ public class Lista01N02 {
 		inputValue = JOptionPane.showInputDialog ("Inform age: ");
 		age = Integer.parseInt(inputValue);
 		inputValue = JOptionPane.showInputDialog("Inform opinion: "
-															+ "\n(1) GREAT"
-															+ "\n(2) GOOD"
-															+ "\n(3) REGULAR"
-															+ "\n(4) BAD"
-															+ "\n(5) TERRIBLE");
+								+ "\n(1) GREAT"
+								+ "\n(2) GOOD"
+								+ "\n(3) REGULAR"
+								+ "\n(4) BAD"
+								+ "\n(5) TERRIBLE");
 		opinion = Integer.parseInt(inputValue);
 		
 		if (opinion == 1) {
