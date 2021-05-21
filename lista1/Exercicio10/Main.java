49ae89be26e5366ad23a8331985899988b5ef695
Laura Vivan Gonçalves <64754203+Laura-a11y@users.noreply.github.com>
2021-05-20 21:25:12 -0300

    Update Main.java

--- lista1/Exercicio10/Main.java
+++ lista1/Exercicio10/Main.java
@@ -4,13 +4,24 @@ public class Main {
 	public static void main(String[] args) {
 		
 		PlacaDeVideo placaVideo = new PlacaDeVideo("Intel Graphics 620", "Modelo Y", 
-				"1000.1", 1);
-		PlacaDeSom placaSom = new PlacaDeSom("Intel Audio", "Modelo Z", "2456.1", 20);
-		DiscoRigido disco = new DiscoRigido("Disco 1", "Modelo X", "001", 100);
+				"4", 1);
+		PlacaDeSom placaSom = new PlacaDeSom("Intel Audio", "Modelo Z", "1", 20);
+		DiscoRigido disco = new DiscoRigido("HD", "Modelo X", "3", 120);
 		
-		disco.usarEspaco(100);
+		placaVideo.mostrarInformacoes();
+		placaVideo.desativar();
+		placaVideo.mostrarStatus();
+		
+		placaSom.mostrarInformacoes();
+		System.out.println(placaSom.isAtivado());
+		placaSom.mostrarStatus();
+		
+		disco.mostrarInformacoes();
+		disco.usarEspaco(58);
 		disco.mostrarStatus();
-
+		disco.liberarEspaco(58);
+		disco.mostrarStatus();
+		disco.usarEspaco(120);
+		disco.mostrarStatus();
 	}
-
 }
