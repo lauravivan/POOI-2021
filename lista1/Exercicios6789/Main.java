07c58afef01c9cd0bcb1fad7266609511ad844a0
Laura Vivan Gonçalves <64754203+Laura-a11y@users.noreply.github.com>
2021-05-20 21:00:46 -0300

    Update Main.java

--- lista1/Exercicios6789/Main.java
+++ lista1/Exercicios6789/Main.java
@@ -3,16 +3,30 @@ public class Main {
 
 	public static void main(String[] args) {
 	
-		Casa casa = new Casa("ROSA");
-		Porta porta2 = new Porta(1, 2, 3, "rosa");
-		Porta porta1 = new Porta(1, 2, 3, "rosa");
-		//Porta porta3 = new Porta(1, 2, 3, "rosa");
+		Casa minhaCasa = new Casa("verde");
+		Porta porta1 = new Porta(3, 1, 42, "laranja");
+		Porta porta2 = new Porta(5.2f, 6, 22, "roxo");
 		
-		casa.setPorta1(porta1);
-		casa.setPorta2(porta2);
-		//casa.setPorta3(porta3);
+		minhaCasa.setPorta1(porta1);
+		minhaCasa.setPorta2(porta2);
 		porta1.abre();
-		System.out.println(casa.quantasPortasEstaoAbertas());
+		System.out.println(minhaCasa.quantasPortasEstaoAbertas());
+		System.out.println(porta1.isAberta());
+		System.out.println(porta2.isAberta());
+		minhaCasa.getPorta1();
+		minhaCasa.getPorta2();
+		
+		Edificio meuEdificio = new Edificio(5, 5, "preto");
+		Porta[] portas = new Porta[meuEdificio.getTotalPortas()];
+		
+		portas[0] = new Porta(4, 6, 80, "azul");
+		portas[1] = new Porta(3, 2, 10, "vermelho");
+		portas[2] = new Porta(5, 3, 1, "amarela");
+		portas[3] = new Porta(7, 4, 1, "verde");
+		portas[4] = new Porta(4, 8, 40, "rosa");
+		
+		meuEdificio.setPortas(portas);
+		meuEdificio.getPortas();
 
 	}
 }
