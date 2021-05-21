7fe77d1167668244964add60c1d65b670baf3038
Laura Vivan Gonçalves <64754203+Laura-a11y@users.noreply.github.com>
2021-05-20 21:47:47 -0300

    Create Main.java

---
+++ EXE4/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+      BancoDoMotorista banco = new BancoDoMotorista();
+      
+      banco.setPosicao(3);
+      banco.getPosicao();
+      banco.isTotalmenteLevantado();
+      banco.isTotalmenteAbaixado();
+      banco.isLevantado();
+      banco.abaixarBanco();
+      banco.isAbaixado();
+      banco.subirBanco();
+      banco.subirBanco();
+      banco.subirBanco();
+      banco.subirBanco();
+      banco.getPosicao();
+    } 
+}
